8b3a4856bf9ca39a9bfa33a0b95d735ac1e42fe6
kuribayashi <tktkchestnut@gmail.com>
2021-08-18 10:40:13 +0900

    fix generator

--- generate_image.py
+++ generate_image.py
@@ -1,11 +1,13 @@
+import csv
 import numpy as np
+import pandas as pd
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 import random
 
 
-MAX_STORY = 6
+MAX_STORY = 8
 MAX_LEVEL = 5
 
 def add_text_to_image(img, text, font_path, font_size, font_color, height, width, max_length=740):
@@ -21,7 +23,7 @@ def add_text_to_image(img, text, font_path, font_size, font_color, height, width
 
     return img
 
-def messages(background, test_day):
+def add_messages(background, test_day):
     title = "Sample text"
     font_path = "fonts/mick-caster/Mick Caster.otf"
     font_size = 57
@@ -85,23 +87,11 @@ def load_image():
     return icons
 
 
-def main():
-    background_path = 'img/input/background.png'
-    background = Image.open(background_path).copy()
-
-    # background color
-    background_color = Image.new('RGB', (1920, 1080), (87,179,110))
-    background.paste(background_color, (0, 0))
-
-    # ground images
-    base_path = 'img/input/base_0.png'
-    base = Image.open(base_path).copy()
-    background.paste(base, (0, 0), base)
+def create_images(story, section, output_csv):
+    message_df = pd.read_csv("src/messages.csv")
 
     # load building images from folder
     icons = load_image()
-    section = [3,1,5,4]
-    story = 2
     section_level = [0,0,0,0]
     total_level = 4 * (MAX_LEVEL-1) # 4sections
 
@@ -109,23 +99,68 @@ def main():
     for i in section:
         story_name += str(i)
 
+    past_levels = [0, 0, 0, 0]
+
     for level in range(total_level):
+        background_path = 'img/input/background.png'
+        background = Image.open(background_path).copy()
+
+        # background color
+        background_color = Image.new('RGB', (1920, 1080), (87,179,110))
+        background.paste(background_color, (0, 0))
+
+        # ground images
+        base_path = 'img/input/base_0.png'
+        base = Image.open(base_path).copy()
+        background.paste(base, (0, 0), base)
+
         selected_section = random.randrange(4)
         while section_level[selected_section] > (MAX_LEVEL-2):
             selected_section = random.randrange(4)
         section_level[selected_section] += 1
 
         # buildings
-        background = paste_image(background, icons, section[0], section_level[0], 895, 260) # upper right
-        background = paste_image(background, icons, section[1], section_level[1], 300, 560) # upper left
-        background = paste_image(background, icons, section[2], section_level[2], 1190, 405) # bottom right
-        background = paste_image(background, icons, section[3], section_level[3], 590, 705) # bottom left
+        background = paste_image(background, icons, section[0], section_level[0], 900, 250) # upper right
+        background = paste_image(background, icons, section[1], section_level[1], 294, 552) # upper left
+        background = paste_image(background, icons, section[2], section_level[2], 1192, 396) # bottom right
+        background = paste_image(background, icons, section[3], section_level[3], 586, 698) # bottom left
         
-        # front decorations
+        message_column = 2
+        value = (section[selected_section]*(MAX_LEVEL-1) + section_level[selected_section] - 1)
+        print(level, "selected_section",selected_section,"current_section=", value)
+        level_message = message_df.iat[value, 1] + "区画のニュースです！ " + message_df.iat[value, message_column]
+        print(level_message)
+        level_population = message_df.iat[value, 6]
+        level_education = message_df.iat[value, 7]
+        level_health = message_df.iat[value, 8]
+        level_nature = message_df.iat[value, 9]
+
+        past_levels[0] += level_population
+        past_levels[1] += level_education
+        past_levels[2] += level_health
+        past_levels[3] += level_nature
+
+        output_csv = output_csv.append({
+            "title_number":story, 
+            "message":level_message, 
+            "level":level, 
+            "character":message_df.iat[value, 3], 
+            "character_message":message_df.iat[value, 4],
+            "character_img":message_df.iat[value, 5],
+            "level_population":past_levels[0],
+            "level_education":past_levels[1],
+            "level_health":past_levels[2],
+            "level_nature":past_levels[3],
+        }, ignore_index=True)
+        
+        print("levels", past_levels)
+
+        # put tree
         trees_path = 'img/input/front_trees.png'
         trees = Image.open(trees_path).copy()
         background.paste(trees, (0, 635), trees)
         
+        # put boards
         boards_path = 'img/input/front_boards.png'
         boards = Image.open(boards_path).copy()
         background.paste(boards, (650, 330), boards)
@@ -133,14 +168,30 @@ def main():
         # put text
         # background = messages(background,title)
 
-        """
-        title = (str(section[0])+"-"+str(section_level[0])+"_"
-                +str(section[1])+"-"+str(section_level[1])+"_"
-                +str(section[2])+"-"+str(section_level[2])+"_"
-                +str(section[3])+"-"+str(section_level[3]))
-        """
         png_filename = 'img/output/story_{}_{}.png'.format(story, level)
         background.save(png_filename, quality=95)
 
+        background = None
+    return output_csv
+    
+
 if __name__=="__main__":
-    main()
+    output = pd.DataFrame(columns=[
+        "title_number", 
+        "message", 
+        "level", 
+        "character", 
+        "character_message",
+        "character_img",
+        "level_population",
+        "level_education",
+        "level_health",
+        "level_nature",
+        ])
+
+    output = create_images(0, [1,0,3,4], output)
+    output = create_images(1, [2,4,5,6], output)
+    output = create_images(2, [0,2,3,4], output)
+    output = create_images(3, [5,1,2,6], output)
+
+    output.to_csv('output.csv', header=False)
